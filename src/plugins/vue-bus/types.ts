e26f8397fcdcb57698ca8148f3dc3b362bd6c4fd
Mikalai <mb@3nsoft.com>
2024-09-29 14:29:51 -0400

    Force explicit null or value in emitting events

--- src/plugins/vue-bus/types.ts
+++ src/plugins/vue-bus/types.ts
@@ -6,7 +6,7 @@ export type CbFunction<T> = (arg: T) => void;
 export interface VueEventBus<T extends object> {
   on: <Ev extends keyof T>(type: Ev, handler: CbFunction<T[Ev]>) => void;
   off: <Ev extends keyof T>(type: Ev, handler?: CbFunction<T[Ev]>) => void;
-  emit: <Ev extends keyof T>(type: Ev, arg?: T[Ev]) => void;
+  emit: <Ev extends keyof T>(type: Ev, arg: T[Ev]) => void;
   once: <Ev extends keyof T>(type: Ev, handler: CbFunction<T[Ev]>) => void;
   clear: () => void;
 }
