8ca177ad9f7af42c8470de0a4661c5de156190a2
Vitaly <vitaliy@uxpressia.com>
2025-12-03 21:42:06 +0300

    Fix Ui3nIcon component

--- src/components/ui3n-icon/ui3n-icon.vue
+++ src/components/ui3n-icon/ui3n-icon.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, onMounted, useTemplateRef, watch } from 'vue';
+import { computed } from 'vue';
 import type { Ui3nIconEmits, Ui3nIconProps } from './types';
 
 const props = withDefaults(
@@ -11,8 +11,6 @@ const props = withDefaults(
   },
 );
 const emits = defineEmits<Ui3nIconEmits>();
-
-const iconEl = useTemplateRef('icon');
 
 const widthVal = computed(() => props.size || props.width || 16);
 const heightVal = computed(() => props.size || props.height || 16);
@@ -35,29 +33,12 @@ const iconStyle = computed(() => {
 function onClick(ev: Event) {
   emits('click', ev);
 }
-
-onMounted(() => {
-  if (iconEl.value) {
-    iconEl.value.classList.add(`ui3n-icon__${props.icon}`);
-  }
-});
-
-watch(
-  () => props.icon,
-  (val, oldVal) => {
-    if (val && val !== oldVal) {
-      iconEl.value?.classList.remove(`ui3n-icon__${oldVal}`);
-      iconEl.value?.classList.add(`ui3n-icon__${val}`);
-    }
-  }
-)
 </script>
 
 <template>
   <div
-    ref="icon"
-    class="ui3n-icon"
-    :class="$style.icon"
+    :class="$style.ui3nIcon"
+    :data-icon="icon"
     :title="title"
     :style="iconStyle"
     @click="onClick"
@@ -65,9 +46,10 @@ watch(
 </template>
 
 <style lang="scss" module>
-.icon {
+.ui3nIcon {
   --ui3n-icon-color: v-bind(color);
 
+  display: block;
   min-width: v-bind(widthCss);
   width: v-bind(widthCss);
   max-width: v-bind(widthCss);
@@ -76,6 +58,10 @@ watch(
   max-height: v-bind(heightCss);
   flex-grow: 0;
   flex-shrink: 0;
+  background-color: currentcolor;
   color: var(--ui3n-icon-color);
+  mask-image: var(--svg);
+  mask-repeat: no-repeat;
+  mask-size: 100% 100%;
 }
 </style>
